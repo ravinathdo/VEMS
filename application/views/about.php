e113cee4e1c95ff70852896f929aa36d299acacd
User <User@DESKTOP-76HRC5D>
2018-11-05 05:31:44 +0530

    drivegreen_db V13

--- application/views/about.php
+++ application/views/about.php
@@ -23,21 +23,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     </head>
     <body>
         <!-- top-header -->
-        <div class="top-header">
-            <div class="container">
-                <ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
-                    <li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
-                    <li class="prnt"><a href="javascript:window.print()">Print/SMS Ticket</a></li>
-
-                </ul>
-                <ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
-                    <li class="tol">Toll Number : 123-4568790</li>				
-                    <li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
-                    <li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>
-                </ul>
-                <div class="clearfix"></div>
-            </div>
-        </div>
+        
         <!--- /top-header ---->
         <!--- header ---->
         <div class="header">
--- application/views/index.php
+++ application/views/index.php
@@ -18,43 +18,11 @@
         <!--//end-animate-->
     </head>
     <body>
-        <!-- top-header -->
-        <div class="top-header">
-            <div class="container">
-<!--                <ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
-                    <li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
-                    <li class="prnt"><a href="javascript:window.print()">Print/SMS Ticket</a></li>
-
-                </ul>-->
-                <ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
-                    <li class="tol">Telephone Number : 123-4568790</li>				
-                    <li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a> </li> 
-                    <!--<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>-->
-                    <li class="sigi"> | <a href="#" data-toggle="modal" data-target="#myModal5" > Drive Green Login</a></li>
-                </ul>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-        <!--- /top-header ---->
-        <!--- header ---->
-        <div class="header">
-            <div class="container">
-                <div class="logo wow fadeInDown animated" data-wow-delay=".5s">
-                    <a href="index">Drive <span>Green</span></a>	
-                </div>
-                <div class="bus wow fadeInUp animated" data-wow-delay=".5s">
-                    <a href="#" data-toggle="modal" data-target="#myModal"  class="buses active">SIGNUP For New Reservation</a>
-                    <!--<a href="hotels.html">HOTELS</a>-->
-                </div>
-                <div class="lock fadeInDown animated" data-wow-delay=".5s"> 
-                    <li><i class="fa fa-leaf"></i></li>
-                    <li><div class="securetxt">CLEAN &amp; PROTECT THE<br>  ENVIRONMENT </div></li>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-        <!--- /header ---->
+        
+        <!--pre_header-->
+        <?php include './_pre_header.php';?>
+        <!--pre_header-->
+        
         <!--- footer-btm ---->
         <div class="footer-btm wow fadeInLeft animated" data-wow-delay=".5s">
             <div class="container">
@@ -114,12 +82,12 @@
             <div class="col-md-7 bann-info wow fadeInRight animated" data-wow-delay=".5s">
                 <h2>Online Bookings its Free</h2>
                 <div class="ban-top">
-                    <div class="bnr-left">
+<!--                    <div class="bnr-left">
                         <label class="inputLabel">From</label>
                         <input class="city" type="text" value="Enter a city" onfocus="this.value = '';" onblur="if (this.value == '') {
                                             this.value = 'Enter a city';
                                         }" required=>
-                    </div>
+                    </div>-->
                      <div class="bnr-right">
                          Register with <a href="#" data-toggle="modal" data-target="#myModal" >
                          Green Drive free
